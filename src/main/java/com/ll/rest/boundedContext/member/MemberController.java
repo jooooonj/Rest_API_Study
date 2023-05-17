1fab92008b3c596fc0a8a106049bde234b49b6eb
jooooonj <lee22424205@naver.com>
2023-05-17 16:29:38 +0900

    Rest API에 필요없는 security 설정 모두 끄기

---
+++ src/main/java/com/ll/rest/boundedContext/member/MemberController.java
@@ -0,0 +1,16 @@
+package com.ll.rest.boundedContext.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    @PostMapping("/login")
+    public String login() {
+        return "성공";
+    }
+}
