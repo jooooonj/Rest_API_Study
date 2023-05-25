1f3c6740a7078fd05b2a5eec1e0334b32667a2fc
jooooonj <lee22424205@naver.com>
2023-05-25 16:28:34 +0900

    권한이 필요한 api에 접근할 수 있도록 설정

--- src/main/java/com/ll/rest/base/springDoc/SpringDocConfig.java
+++ src/main/java/com/ll/rest/base/springDoc/SpringDocConfig.java
@@ -2,10 +2,20 @@ package com.ll.rest.base.springDoc;
 
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
+//swagger 설정
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "REST API", version = "v1"))
+@OpenAPIDefinition(info = @Info(title = "REST API", version = "v1")) //큰 제목 커스터마이징
+@SecurityScheme( // 로그인 권한이 필요한 API의 경우에 swagger에서 토큰을 통해 권한을 받을 수 있게끔 설정
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class SpringDocConfig {
+
 }
--- src/main/java/com/ll/rest/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/rest/boundedContext/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.ll.rest.boundedContext.member.dto.MemberDto;
 import com.ll.rest.boundedContext.member.entity.Member;
 import com.ll.rest.boundedContext.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -56,7 +57,7 @@ public class MemberController {
     }
 
     @GetMapping(value ="/me", consumes=ALL_VALUE)
-    @Operation(summary = "로그인된 사용자의 정보")
+    @Operation(summary = "로그인된 사용자의 정보", security = @SecurityRequirement(name = "bearerAuth"))
     public RsData<MeResponse> me(@AuthenticationPrincipal User user) {
         Member member = memberService.findByUsername(user.getUsername()).get();
 
