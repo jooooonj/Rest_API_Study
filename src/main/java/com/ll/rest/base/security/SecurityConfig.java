7f7b86077c141c0987d2c44c04a2fa6b2f4bea7c
jooooonj <lee22424205@naver.com>
2023-05-25 18:10:58 +0900

    스웨거 접근 권한은 admin만 가지도록 설정

--- src/main/java/com/ll/rest/base/security/SecurityConfig.java
+++ src/main/java/com/ll/rest/base/security/SecurityConfig.java
@@ -1,55 +1,34 @@
 package com.ll.rest.base.security;
 
-import com.ll.rest.base.security.entryPoint.ApiAuthenticationEntryPoint;
-import com.ll.rest.base.security.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final JwtAuthorizationFilter jwtAuthorizationFilter;
-    private final ApiAuthenticationEntryPoint authenticationEntryPoint;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .securityMatcher("/api/**") // 아래의 모든 설정은 /api/** 경로에만 적용
-                // 시큐리티의 접근권한 예외를 핸들링
-                .exceptionHandling(exceptionHandling -> exceptionHandling
-                        .authenticationEntryPoint(authenticationEntryPoint)
+                .formLogin().and()
+                .logout(
+                        logout -> logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                                .logoutSuccessUrl("/swagger-ui.html")
                 )
                 .authorizeHttpRequests(
                         authorizeHttpRequests -> authorizeHttpRequests
-                                .requestMatchers(HttpMethod.POST, "/api/*/member/login").permitAll() // 로그인은 누구나 가능
-                                .requestMatchers(HttpMethod.GET, "/api/*/articles").permitAll() // 게시글 보기는 누구나 가능
-                                .requestMatchers(HttpMethod.GET,"/api/*/articles/*").permitAll()
-                                .anyRequest().authenticated() // 나머지는 인증된 사용자만 가능
-                )
-                .cors().disable() // 타 도메인에서 API 호출 가능
-                .csrf().disable() // CSRF 토큰 끄기
-                .httpBasic().disable() // httpBaic 로그인 방식 끄기
-                .formLogin().disable() // 폼 로그인 방식 끄기
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(STATELESS)
-                ) // 세션끄기
-                //jwt 토큰 확인하도록
-                .addFilterBefore(
-                        jwtAuthorizationFilter,
-                        UsernamePasswordAuthenticationFilter.class
+                                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
+                                .hasAuthority("ADMIN")
+                                .anyRequest()
+                                .permitAll()
                 );
 
         return http.build();
     }
-
 }
